d2d877b74e66e9395394d2be440829ae9ced9696
gmanka <gmankab@gmail.com>
2025-10-11 20:58:23 +0500

    [u] breaking changes: rename .raw images

--- partition-images/postprocess.sh
+++ partition-images/postprocess.sh
@@ -6,23 +6,23 @@ mkdir images
 
 sudo kpartx -vafs "./$FULL_IMAGE/image/disk.raw"
 sudo dd if=/dev/mapper/loop0p1 of=efipart.vfat bs=1M
-sudo dd if=/dev/mapper/loop0p2 of=images/boot.raw bs=1M
-sudo dd if=/dev/mapper/loop0p3 of=images/root.raw bs=1M
+sudo dd if=/dev/mapper/loop0p2 of=images/fedora_boot.raw bs=1M
+sudo dd if=/dev/mapper/loop0p3 of=images/fedora_rootfs.raw bs=1M
 
 VOLID=$(file efipart.vfat | grep -Eo "serial number 0x.{8}" | cut -d' ' -f3)
-truncate -s $ESP_SIZE images/esp.raw
-mkfs.vfat -F 32 -S 4096 -n EFI -i $VOLID images/esp.raw
+truncate -s $ESP_SIZE images/fedora_esp.raw
+mkfs.vfat -F 32 -S 4096 -n EFI -i $VOLID images/fedora_esp.raw
 
 mkdir -p esp.old esp.new
 sudo mount -o loop efipart.vfat esp.old
-sudo mount -o loop images/esp.raw esp.new
+sudo mount -o loop images/fedora_esp.raw esp.new
 
 sudo cp -a esp.old/. esp.new/
 sudo umount esp.old/
 
 if [ "$INSTALL_DTB" = "true" ]; then
     mkdir boot
-    sudo mount -o loop images/boot.raw boot
+    sudo mount -o loop images/fedora_boot.raw boot
 
     sudo cp -ar boot/ostree/default-*/dtb esp.new/dtb
 
@@ -36,4 +36,4 @@ rmdir esp.old esp.new
 sudo chmod 666 images/*
 
 # pad the last block to 4096 bytes
-dd if=/dev/zero bs=1 count=512 | tee -a images/root.raw
+dd if=/dev/zero bs=1 count=512 | tee -a images/fedora_rootfs.raw
